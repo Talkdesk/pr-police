0ceabd5a716a8681a5468cb62f4e7f58b10be2c1
Clif Bergmann <cbergmann@familysearch.org>
2017-09-05 19:54:06 -0600

    docs: add TZ env variable

--- test/server.js
+++ test/server.js
@@ -37,14 +37,3 @@ test('it calls slackbots onStart handler', (t) => {
   clock.tick(envMock.CHECK_INTERVAL)
   t.ok(SlackbotsMock.prototype.on.calledWith('start'))
 })
-
-// DISABLED, SINCE CHECKING IS NOW GATED BY DAYS AND TIMES
-// test('it calls pullhub on start', (t) => {
-//   t.plan(1)
-
-//   process.env = envMock
-
-//   server()
-//   clock.tick(envMock.CHECK_INTERVAL)
-//   t.ok(pullhubMock.called)
-// })
