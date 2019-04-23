d9d848ec0c9cf41a8c07a2214c6e5e08f54ba939
Diogo Osório <diogo.osorio@talkdesk.com>
2019-04-24 00:02:37 +0100

    feat: makes the server work with more than one project configuration
    This commit changes the server implementation so that a single instance
    is able to probe multiple projects, each with a set of repositories,
    tags and Slack channels to where the message should be published.
    
    A couple of noteworthy details:
    
    * The implementation is extremely naive and can be optimized (i.e. if
    the same repository is present in multiple projects, Github will be
    probed n times, instead of only 1), but I think that it's good enough
    for a first iteration
    
    * I had a very had time writing proper tests for the feature, I simply
    kept the same logic that was already there (I reckon to properly test
    this a bigger refactor needs to happen)

--- test/server.js
+++ test/server.js
@@ -1,48 +1,65 @@
 const test = require('tape')
-const proxyquire = require('proxyquire')
 const sinon = require('sinon')
+const server = require('../lib/server')
 
-const SlackbotsMock = function SlackbotsMock () {}
-SlackbotsMock.prototype.on = sinon.stub()
-SlackbotsMock.prototype.on.withArgs('start').yields(true)
-
-const envMock = {
-  SLACK_TOKEN: 'foo',
-  GH_TOKEN: 'foo',
-  SLACK_CHANNELS: 'foo',
-  GH_REPOS: 'foo',
-  CHECK_INTERVAL: '1'
+const botMock = {on: sinon.stub().yields(true)}
+const pullhubMock = sinon.stub().resolves([{title: 'PR title', html_url: 'http://github.com/talkdesk/project/pull/23'}])
+const clock = sinon.useFakeTimers()
+const configuration = {
+  tokens: {
+    github: 'github-token',
+    slack: 'slack-token'
+  },
+  polling: {
+    interval: 500
+  },
+  projects: [
+    {
+      repository: {
+        names: ['repo-1', 'repo-2'],
+        labels: ['label-1']
+      },
+      slack: {
+        channels: ['channel-1', 'channel-2'],
+        groups: ['group-1'],
+        bot: {
+          name: 'bot name',
+          icon: 'bot icon'
+        }
+      }
+    },
+    {
+      repository: {
+        names: ['repo-3', 'repo-4'],
+        labels: ['label-2']
+      },
+      slack: {
+        channels: ['channel-3', 'channel-4'],
+        groups: ['group-2'],
+        bot: {
+          name: 'bot name 2',
+          icon: 'bot icon 2'
+        }
+      }
+    }
+  ]
 }
 
-const pullhubMock = sinon.stub().resolves([])
-const clock = sinon.useFakeTimers()
+test('it calls slackbot handlers', (assert) => {
+  server(configuration, botMock)
 
-const server = proxyquire('../lib/server', {
-  slackbots: SlackbotsMock,
-  pullhub: pullhubMock
+  assert.ok(botMock.on.calledWith('start'))
+  assert.ok(botMock.on.calledWith('message'))
+  assert.ok(botMock.on.calledWith('error'))
+  assert.end()
 })
 
-test('it throws error on missing required env var', (t) => {
-  t.plan(1)
+test('it calls pullhub on start', (assert) => {
+  server(configuration, botMock, pullhubMock)
 
-  t.throws(server, Error)
-})
+  clock.tick(500)
 
-test('it calls slackbots onStart handler', (t) => {
-  t.plan(1)
-
-  process.env = envMock
-
-  server()
-  t.ok(SlackbotsMock.prototype.on.calledWith('start'))
-})
-
-test('it calls pullhub on start', (t) => {
-  t.plan(1)
-
-  process.env = envMock
-
-  server()
-  clock.tick(envMock.CHECK_INTERVAL)
-  t.ok(pullhubMock.called)
+  assert.ok(pullhubMock.calledWith(['repo-1', 'repo-2'], 'label-1'))
+  assert.ok(pullhubMock.calledWith(['repo-3', 'repo-4'], 'label-2'))
+  assert.end()
 })
