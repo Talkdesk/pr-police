46929fd57dd585e0dce2c40416d8e15a51d421aa
Diogo Osório <diogo.osorio@talkdesk.com>
2019-04-24 00:54:42 +0100

    feat: load configuration from a file
    This commit changes the `configuration` module in such a way that it's
    now possible to provide a file path with the project configuration and
    load the application configuration from there.
    
    If the file path argument isn't provided, the previous "env var based"
    behaviour is assumed (as to not break current applications using that
    strategy).
    
    If the file path is provided, the project related env vars are not
    evaluated and instead the config file contents is used.

--- test/configuration.js
+++ test/configuration.js
@@ -1,7 +1,12 @@
+const path = require('path')
 const test = require('tape')
 const configuration = require('../lib/configuration')
 
-test('configuration parsing', function (tc) {
+const testDataDir = path.join(path.basename(__dirname), 'data')
+const configFile = path.join(testDataDir, 'config.json')
+const invalidConfigFile = path.join(testDataDir, 'invalid_config.json')
+
+test('environment configuration', function (tc) {
   tc.test('environment variables parsing', function (assert) {
     const environment = {
       SLACK_TOKEN: 'slack-token',
@@ -83,6 +88,77 @@ test('configuration parsing', function (tc) {
 
   tc.test('missing required configuration', function (assert) {
     assert.throws(function () { configuration.parse({}) }, /ConfigurationError/)
+    assert.end()
+  })
+})
+
+test('file configuration', function (tc) {
+  tc.test('non-existing file', function (assert) {
+    assert.throws(function () { configuration.parse({}, './non-existing') }, /ConfigurationError/)
+    assert.end()
+  })
+
+  tc.test('missing tokens', function (assert) {
+    assert.throws(function () { configuration.parse({}, configFile) }, /ConfigurationError/)
+    assert.end()
+  })
+
+  tc.test('invalid configuration file', function (assert) {
+    const env = {
+      GH_TOKEN: 'github-token',
+      SLACK_TOKEN: 'slack-token'
+    }
+
+    assert.throws(function () { configuration.parse(env, invalidConfigFile) }, /ConfigurationError/)
+    assert.end()
+  })
+
+  tc.test('valid configuration file', function (assert) {
+    const env = {
+      GH_TOKEN: 'github-token',
+      SLACK_TOKEN: 'slack-token',
+      CHECK_INTERVAL: 5000
+    }
+
+    assert.deepEquals(configuration.parse(env, configFile), {
+      tokens: {
+        github: 'github-token',
+        slack: 'slack-token'
+      },
+      polling: {
+        interval: 5000
+      },
+      projects: [
+        {
+          repository: {
+            names: ['talkdesk/repo1', 'talkdesk/repo2'],
+            labels: ['label1', 'label2']
+          },
+          slack: {
+            channels: ['channel1', 'channel2'],
+            groups: ['group1', 'group2'],
+            bot: {
+              name: 'bot_name',
+              icon: 'https://images.talkdesk.com/bot.png'
+            }
+          }
+        },
+        {
+          repository: {
+            names: ['talkdesk/repo3', 'talkdesk/repo4'],
+            labels: ['label3', 'label4']
+          },
+          slack: {
+            channels: ['channel3', 'channel4'],
+            groups: ['group3', 'group4'],
+            bot: {
+              name: 'other_bot_name',
+              icon: 'https://images.talkdesk.com/other_bot.png'
+            }
+          }
+        }
+      ]
+    })
     assert.end()
   })
 })
