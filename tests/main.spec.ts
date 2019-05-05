6f7e54bda00055c031b5577ecc482fd7b43e6695
Jorge Rebocho <jorge.rebocho@talkdesk.com>
2019-05-05 11:57:58 +0100

    Include tests in linter script

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,7 +1,7 @@
-import {sum} from '../src/main'
+import { sum } from '../src/main'
 
 describe('sum', () => {
   it('sums two numbers', () => {
-    expect(sum(1,2)).toEqual(3)
+    expect(sum(1, 2)).toEqual(3)
   })
 })
